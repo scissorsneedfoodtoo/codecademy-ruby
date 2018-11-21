7627d9f1981772b69d6597fd65f65e9fedd47db4
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2018-11-21 20:05:51 +0900

    Renamed file and refactored code

--- create-a-histogram.rb
+++ create-a-histogram.rb
@@ -1,15 +1,9 @@
 puts "Let's hash it out. Give me a sentence: "
 text = gets.chomp.downcase.gsub(/[[:punct:]]/, '')
 
-text_arr = text.split(" ")
-
-sentence_hash = {}
-text_arr.each do |word|
-  if sentence_hash[word]
-    sentence_hash[word] += 1
-  elsif word.length > 0
-    sentence_hash[word] = 1
-  end
-end
-
-sentence_hash.each{ |word, count| puts "#{word}: #{count}"}
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each{ |word| frequencies[word] += 1 }
+requencies = frequencies.sort_by{ |word, count| count }
+frequencies.reverse!
+frequencies.each{ |word, count| puts "#{word}: #{count}"}
